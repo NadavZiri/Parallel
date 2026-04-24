33fda8e7f38074e6d937893cc74d8bf493100807
NadavZiri <nadavziri123@gmail.com>
2026-04-24 16:58:20 +0300

    finished decode

--- Assignments/Semester_B/Assignment1/decode/decode.c
+++ Assignments/Semester_B/Assignment1/decode/decode.c
@@ -1,6 +1,8 @@
 long decode_c_version(long x, long y, long z) {
     y = y - z;
     x = x * y;
-    x = -(x & 1);
-    return x;
+    y = y << 63;
+    y = y >> 63;
+    y = y ^ x;
+    return y;
 };
